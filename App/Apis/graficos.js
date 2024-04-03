c2753918037e56bb465ba35f9afffc35e12ff6b5
Xavier <xavier.e.santamaria.ramirez@gmail.com>
2024-04-03 08:21:26 -0500

    Insert Cliente

--- App/Apis/graficos.js
+++ App/Apis/graficos.js
@@ -1,40 +1,45 @@
 //grafico 2
-const barrasG = document.getElementById('barras');
-new Chart(barrasG,{
-    type: 'bar',
-    data:  {
-        labels: ['Mountain Bike Socks,M',	   
-        'Road-550-W Yellow,40 ',
-        'HL Mountain Pedal',	      
-        'HL Mountain Pedal',	      ],
-        datasets: [{
-          label: 'Rating',
-          data: [5,5,4,2],
-          backgroundColor: [
-            'rgba(255, 99, 132, 0.2)',
-            'rgba(255, 159, 64, 0.2)',
-            'rgba(255, 205, 86, 0.2)',
-            'rgba(75, 192, 192, 0.2)',
-            'rgba(54, 162, 235, 0.2)',
-            'rgba(153, 102, 255, 0.2)',
-            'rgba(201, 203, 207, 0.2)'
-          ],
-          borderColor: [
-            'rgb(255, 99, 132)',
-            'rgb(255, 159, 64)',
-            'rgb(255, 205, 86)',
-            'rgb(75, 192, 192)',
-            'rgb(54, 162, 235)',
-            'rgb(153, 102, 255)',
-            'rgb(201, 203, 207)'
-          ],
-          borderWidth: 2
-        }],
+const barrasG = document.getElementById("barras");
+new Chart(barrasG, {
+  type: "bar",
+  data: {
+    labels: [
+      "Mountain Bike Socks,M",
+      "Road-550-W Yellow,40 ",
+      "HL Mountain Pedal",
+      "HL Mountain Pedal",
+    ],
+    datasets: [
+      {
+        label: "Rating",
+        data: [5, 5, 4, 2],
+        backgroundColor: [
+          "rgba(255, 99, 132, 0.2)",
+          "rgba(255, 159, 64, 0.2)",
+          "rgba(255, 205, 86, 0.2)",
+          "rgba(75, 192, 192, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(153, 102, 255, 0.2)",
+          "rgba(201, 203, 207, 0.2)",
+        ],
+        borderColor: [
+          "rgb(255, 99, 132)",
+          "rgb(255, 159, 64)",
+          "rgb(255, 205, 86)",
+          "rgb(75, 192, 192)",
+          "rgb(54, 162, 235)",
+          "rgb(153, 102, 255)",
+          "rgb(201, 203, 207)",
+        ],
+        borderWidth: 2,
+      },
+    ],
     options: {
-        scales: {
-          y: {
-            beginAtZero: true
-          }
-        }
-      },  
-    }});
+      scales: {
+        y: {
+          beginAtZero: true,
+        },
+      },
+    },
+  },
+});
--- App/script.js
+++ App/script.js
@@ -1,9 +1,18 @@
 import { pool } from "./Database/conexcionPostgre.js";
 
-const getClientes = async () => {
+export const getClientes = async () => {
   try {
     const result = await pool.query("select * from cliente c ;");
     console.log(result.rows);
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+const insertClientes = async () => {
+  try {
+    const result = await pool.query("");
+    console.log(result);
   } catch (err) {
     console.error(err);
   }
