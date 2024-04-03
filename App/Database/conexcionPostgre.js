2decb7a090ed331f20ff4723d6dfb993ffc57363
Xavier <xavier.e.santamaria.ramirez@gmail.com>
2024-04-02 22:22:43 -0500

    conexcion resuelta

--- App/Database/conexcionPostgre.js
+++ App/Database/conexcionPostgre.js
@@ -1,10 +1,9 @@
 import pg from "pg";
 
-//
-export const pool = new pg.pool({
-    host:'localhost',
-    port:'5432',
-    database:'desarrolloMarket',
-    user:'postgres',
-    password:'xavi-postgre'
-})
+export const pool = new pg.Pool({
+  host: "localhost",
+  port: "5432",
+  database: "desarrolloMarket",
+  user: "postgres",
+  password: "xavi-postgre",
+});
--- App/script.js
+++ App/script.js
@@ -1,12 +1,12 @@
-import { pool } from "./Database/conexcionPostgre";
+import { pool } from "./Database/conexcionPostgre.js";
 
-const getClientes=async()=>{
-    try{
-       const result = await pool.query("select * from cliente c ;");
-       console.log(result)
-    }catch(err){
-        console.error(err)
-    }
-}
+const getClientes = async () => {
+  try {
+    const result = await pool.query("select * from cliente c ;");
+    console.log(result.rows);
+  } catch (err) {
+    console.error(err);
+  }
+};
 
 getClientes();
